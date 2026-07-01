065e77105b9951efcfbc9fa990c19c1abb0bd5bb
Shayem <skshayem@intellicar.in>
2026-07-01 15:20:56 +0530

    feat: FOTA checks version inline from binary, no separate version.json fetch

--- esp32/main/fota.h
+++ esp32/main/fota.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#define FOTA_VERSION_URL \
-    "https://raw.githubusercontent.com/skshayem-ctrl/invo-display/master/esp32/version.json"
+/* Fixed-name asset uploaded to every GitHub release.
+ * /releases/latest/download/ always resolves to the newest release. */
+#define FOTA_BIN_URL \
+    "https://github.com/skshayem-ctrl/invo-display/releases/latest/download/invo-esp32.bin"
 
 #include "../../src/fota.h"
